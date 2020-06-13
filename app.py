f68dca5818b5ab76799d1c1edae8f1348fe34b91
Peter Komaromi <kompeet1@gmail.com>
2020-06-13 13:25:49 +0000

    app modified

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
 
 mongo = PyMongo(app)
 
@@ -21,6 +21,4 @@ def index():
 
 
 if __name__ == '__main__':
- app.run(host=os.environ.get('IP'),
- port=int(os.environ.get('PORT')),
- debug=True)
+    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
