6fe58e8c166daaa058402972a82f1c27ce1a6b67
Peter Komaromi <kompeet1@gmail.com>
2020-07-01 13:46:39 +0000

    Edited heroku config vars

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask_pymongo import PyMongo
 import os
 from os import path
 from flask import Flask
-if path.exists("env.py"):
+if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
+
 
 mongo = PyMongo(app)
 
@@ -17,7 +17,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return "Na, mostmar mindjart kesz! =)"
 
 
 if __name__ == '__main__':
