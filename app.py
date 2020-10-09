9e62a92d0041d68d5914e061006dd1fbfa046ad2
Peter Komaromi <kompeet1@gmail.com>
2020-10-09 13:00:28 +0000

    add reciped added to app.py

--- app.py
+++ app.py
@@ -14,9 +14,15 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
 @app.route('/')
+
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
+
+@app.route('/add_recipes')
+def add_recipes():
+    return render_template('addrecipes.html',
+    categories=mongo.db.categories.find())
 
 
 if __name__ == '__main__':
