88cc01e28f791de38de012d8dc9438234e7a6b6e
Peter Komaromi <kompeet1@gmail.com>
2020-10-10 14:50:23 +0000

    Edit recipe function added to app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #Import installed libraries and components to the Python file
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 import os
 from os import path
 from flask import Flask, render_template, redirect, request, url_for
@@ -28,7 +29,7 @@ def get_recipes():
 
 #Add new recipe using a form, submiting it using POST method
 @app.route('/add_recipe')
-def add_recipes():
+def add_recipe():
     return render_template('addrecipes.html',
     categories=mongo.db.categories.find())
     
@@ -38,6 +39,15 @@ def insert_recipe():
     recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
+
+
+#Find a recipe by its ID then render the editrecipe HTML file
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('editrecipe.html', recipe=the_recipe,
+                           categories=all_categories)    
 
 
 #Host and Port set
