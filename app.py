21a7b7b377c5a39d382dd1fe73834402852b9115
Peter Komaromi <kompeet1@gmail.com>
2020-10-15 18:45:29 +0000

    Beautifed, comments added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 import os
 from os import path
+import re
 from flask import Flask, render_template, redirect, request, url_for
 if os.path.exists("env.py"):
     import env
@@ -13,20 +14,20 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-
-#App's paths, functions helping to let the user "do CRUD":create, retrieve, update, and delete recipes
-
 mongo = PyMongo(app)
 DB = mongo.db
+#App's paths, functions helping to let the user "do CRUD":create, retrieve, update, and delete recipes
 
 
-# Homepage
+
+
+#Homepage
 @app.route('/')
 def home():
     all_categories = list(mongo.db.categories.find())
     return render_template("index.html", categories=all_categories)
 
-# Find recipes
+#Find recipes
 """Search for recipes with regex method,
     results of the search returned with the for loop in results.html"""
 @app.route("/find_recipes")
@@ -38,21 +39,21 @@ def find_recipes():
         {"ingredients": {"$regex": query}})
     all_categories = list(mongo.db.categories.find())
     return render_template(
-        "results.html",
+        "result.html",
         categories=all_categories,
         search=search_term, no_of_docs=no_of_docs)
 
 
 @app.route('/get_recipes')
 def get_recipes():
-    # List of the recipes with find() method
+    #List of the recipes with find() method
     all_categories = list(mongo.db.categories.find())
     return render_template(
         "recipes.html",
         categories=all_categories, recipes=mongo.db.recipes.find())
         
 
-# Add new recipe using a form, submiting it using POST method
+#Add new recipe using a form, submiting it using POST method
 @app.route('/add_recipe')
 def add_recipe():
     all_categories = list(mongo.db.categories.find())
@@ -68,7 +69,7 @@ def insert_recipe():
     return redirect(url_for('get_recipes'))
 
 
-# Find a recipe by its ID then render the editrecipe HTML file
+#Find a recipe by its ID then render the editrecipe HTML file
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
     the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})  
@@ -77,7 +78,7 @@ def edit_recipe(recipe_id):
                            categories=all_categories)    
 
 
-# Update the article after editing it using JSON
+#Update the article after editing it using JSON
 @app.route('/update_recipe/<recipe_id>', methods=["POST"])
 def update_recipe(recipe_id):
     recipes = mongo.db.recipes
@@ -89,17 +90,17 @@ def update_recipe(recipe_id):
         'category_name': request.form.get('category_name'),
         'picture':request.form.get('picture')
     })
-# When the recipe is updated, redirect the user to the list of recipes
+#When the recipe is updated, redirect the user to the list of recipes
     return redirect(url_for('get_recipes'))
 
 
-# Delete recipe selected by its ID then redirect the user to the list of recipes
+#Delete recipe selected by its ID then redirect the user to the list of recipes
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
     mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
     return redirect(url_for('get_recipes'))
 
-# Get Categories
+#Get Categories
 """  Get and display categories, so the user can click on the category_name
     and the list of the recipes"""
 @app.route('/get_categories')
@@ -115,7 +116,7 @@ def display_categories(category_name):
     return render_template('displaycategories.html',  recipes=all_recipe, category=category_name,
                            categories=all_categories)
 
-# Displaying cards with split method
+#Displaying cards with split method
 @app.route('/display_recipe/<recipe_id>')
 def display_recipe(recipe_id):
     the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
@@ -127,6 +128,6 @@ def display_recipe(recipe_id):
         categories=all_categories, ingredients=ingredients, steps=steps)
 
 
-# Host and Port set
+#Host and Port set
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
